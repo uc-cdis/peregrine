0783234d1e3843ce8b73678f58d6190d50610f4d
philloooo <phillis.tt@gmail.com>
2018-02-12 16:07:27 -0600

    chore(test): generate jwt keys in travis

--- peregrine/test_settings.py
+++ peregrine/test_settings.py
@@ -60,9 +60,9 @@ OAUTH2 = {
     "redirect_uri": "",
 }
 
-USER_API = "localhost"
+USER_API = "http://localhost"
 # used by fence.jwt.token.generate_signed_access_token for iss
-BASE_URL = "localhost"
+BASE_URL = "http://localhost"
 
 VERIFY_PROJECT = False
 AUTH_SUBMISSION_LIST = False
@@ -71,9 +71,5 @@ JWT_KEYPAIR_FILES = OrderedDict([
     (
         'key-test',
         ('resources/keys/test_public_key.pem', 'resources/keys/test_private_key.pem'),
-    ),
-    (
-        'key-test-2',
-        ('resources/keys/test_public_key_2.pem', 'resources/keys/test_private_key_2.pem'),
-    ),
+    )
 ])
