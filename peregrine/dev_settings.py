153a7a4b0b6cff9d1306078c00531e6efc80bd01
Giang Bui <giangbui@uchicago.edu>
2018-03-05 11:11:23 -0600

    fix(dev_settings): put back dev_settings

--- peregrine/dev_settings.py
+++ peregrine/dev_settings.py
@@ -70,8 +70,7 @@ PEREGRINE_PORT = int(os.getenv("PEREGRINE_PORT", "5000"))
 # ``random.SystemRandom()``
 FLASK_SECRET_KEY = 'eCKJOOw3uQBR5pVDz3WIvYk3RsjORYoPRdzSUNJIeUEkm1Uvtq'
 
-#DICTIONARY_URL = os.environ.get('DICTIONARY_URL','https://s3.amazonaws.com/dictionary-artifacts/datadictionary/develop/schema.json')
-DICTIONARY_URL = os.environ.get('DICTIONARY_URL','https://s3.amazonaws.com/dictionary-artifacts/kf-dictionary/kf-v0.1.2/schema.json')
+DICTIONARY_URL = os.environ.get('DICTIONARY_URL','https://s3.amazonaws.com/dictionary-artifacts/datadictionary/develop/schema.json')
 
 HMAC_ENCRYPTION_KEY = os.environ.get('CDIS_HMAC_ENCRYPTION_KEY', '')
 OAUTH2 = {
