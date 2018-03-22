67d8abca1bdca0429f3ef7e7b42b5572366500ed
Giang Bui <giangbui@uchicago.edu>
2018-03-22 11:03:16 -0500

    chore(dev_settings): keep config that run locally

--- peregrine/dev_settings.py
+++ peregrine/dev_settings.py
@@ -2,12 +2,6 @@ import os
 from boto.s3.connection import OrdinaryCallingFormat
 from os import environ as env
 
-# Signpost
-SIGNPOST = {
-   'host': env.get('SIGNPOST_HOST', 'http://localhost:8888'),
-   'version': 'v0',
-   'auth': None}
-
 # Auth
 AUTH = 'https://gdc-portal.nci.nih.gov/auth/keystone/v3/'
 INTERNAL_AUTH = env.get('INTERNAL_AUTH', 'https://gdc-portal.nci.nih.gov/auth/')
@@ -49,10 +43,10 @@ PSQLGRAPH = {
     'host': os.getenv("GDC_PG_HOST", "localhost"),
     'user': os.getenv("GDC_PG_USER", "test"),
     'password': os.getenv("GDC_PG_PASSWORD", "test"),
-    'database': os.getenv("GDC_PG_DBNAME", "automated_test")
+    'database': os.getenv("GDC_PG_DBNAME", "sheepdog_automated_test")
 }
 
-PSQL_USER_DB_NAME = 'userapi'
+PSQL_USER_DB_NAME = 'fence_test'
 PSQL_USER_DB_USERNAME = 'test'
 PSQL_USER_DB_PASSWORD = 'test'
 PSQL_USER_DB_HOST = 'localhost'
@@ -72,12 +66,31 @@ FLASK_SECRET_KEY = 'eCKJOOw3uQBR5pVDz3WIvYk3RsjORYoPRdzSUNJIeUEkm1Uvtq'
 
 DICTIONARY_URL = os.environ.get('DICTIONARY_URL','https://s3.amazonaws.com/dictionary-artifacts/datadictionary/develop/schema.json')
 
+OIDC_ISSUER = 'http://localhost/user'
+
 HMAC_ENCRYPTION_KEY = os.environ.get('CDIS_HMAC_ENCRYPTION_KEY', '')
+#OAUTH2 = {
+#    "client_id": os.environ.get('CDIS_PEREGRINE_CLIENT_ID'),
+#    "client_secret": os.environ.get("CDIS_PEREGRINE_CLIENT_SECRET"),
+#    "oauth_provider": os.environ.get("CDIS_USER_API_OAUTH", 'http://localhost:8000/oauth2/'),
+#    "redirect_uri": os.environ.get("CDIS_PEREGRINE_OAUTH_REDIRECT", 'localhost:5000/v0/oauth2/authorize'),
+#}
+
 OAUTH2 = {
-    "client_id": os.environ.get('CDIS_PEREGRINE_CLIENT_ID'),
-    "client_secret": os.environ.get("CDIS_PEREGRINE_CLIENT_SECRET"),
-    "oauth_provider": os.environ.get("CDIS_USER_API_OAUTH", 'http://localhost:8000/oauth2/'),
-    "redirect_uri": os.environ.get("CDIS_PEREGRINE_OAUTH_REDIRECT", 'localhost:5000/v0/oauth2/authorize'),
+    'client_id': '',
+    'client_secret': '',
+    'api_base_url': 'http://localhost/user/',
+    'authorize_url': 'http://localhost/user/oauth2/authorize',
+    'access_token_url': 'http://localhost0/user/oauth2/token',
+    'refresh_token_url': 'http://localhost/user/oauth2/token',
+    'client_kwargs': {
+        'redirect_uri': 'http://localhost/api/v0/oauth2/authorize',
+        'scope': 'openid data user',
+    },
+    # deprecated key values, should be removed after all commons use new oidc
+    'internal_oauth_provider': 'http://localhost/oauth2/',
+    'oauth_provider': 'http://localhost/user/oauth2/',
+    'redirect_uri': 'http://localhost/api/v0/oauth2/authorize'
 }
 
 USER_API = "http://localhost:8000/"
