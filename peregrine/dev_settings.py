3b28c36cb94adca3155fe92267f2ccb13a08540f
Steven Cui <stevencui729@gmail.com>
2018-06-22 12:04:32 -0500

    chore(dev_settings): updated example dev settings to align with local environment setup, including changing default PEREGRINE_PORT to 4000

--- peregrine/dev_settings.py
+++ peregrine/dev_settings.py
@@ -2,12 +2,6 @@ import os
 from boto.s3.connection import OrdinaryCallingFormat
 from os import environ as env
 
-# Signpost
-SIGNPOST = {
-   'host': env.get('SIGNPOST_HOST', 'http://localhost:8888'),
-   'version': 'v0',
-   'auth': None}
-
 # Auth
 AUTH = 'https://gdc-portal.nci.nih.gov/auth/keystone/v3/'
 INTERNAL_AUTH = env.get('INTERNAL_AUTH', 'https://gdc-portal.nci.nih.gov/auth/')
@@ -52,7 +46,7 @@ PSQLGRAPH = {
     'database': os.getenv("GDC_PG_DBNAME", "automated_test")
 }
 
-PSQL_USER_DB_NAME = 'userapi'
+PSQL_USER_DB_NAME = 'fence'
 PSQL_USER_DB_USERNAME = 'test'
 PSQL_USER_DB_PASSWORD = 'test'
 PSQL_USER_DB_HOST = 'localhost'
@@ -63,14 +57,14 @@ PSQL_USER_DB_CONNECTION = "postgresql://{name}:{password}@{host}/{db}".format(
 
 # API server
 PEREGRINE_HOST = os.getenv("PEREGRINE_HOST", "localhost")
-PEREGRINE_PORT = int(os.getenv("PEREGRINE_PORT", "5000"))
+PEREGRINE_PORT = int(os.getenv("PEREGRINE_PORT", "4000"))
 
 # FLASK_SECRET_KEY should be set to a secure random string with an appropriate
 # length; 50 is reasonable. For the random generation to be secure, use
 # ``random.SystemRandom()``
 FLASK_SECRET_KEY = 'eCKJOOw3uQBR5pVDz3WIvYk3RsjORYoPRdzSUNJIeUEkm1Uvtq'
 
-DICTIONARY_URL = os.environ.get('DICTIONARY_URL','https://s3.amazonaws.com/dictionary-artifacts/datadictionary/develop/schema.json')
+DICTIONARY_URL = os.environ.get('DICTIONARY_URL')
 
 HMAC_ENCRYPTION_KEY = os.environ.get('CDIS_HMAC_ENCRYPTION_KEY', '')
 OAUTH2 = {
@@ -80,7 +74,7 @@ OAUTH2 = {
     "redirect_uri": os.environ.get("CDIS_PEREGRINE_OAUTH_REDIRECT", 'localhost:5000/v0/oauth2/authorize'),
 }
 
-USER_API = "http://localhost:8000/"
+USER_API = "http://localhost/user"
 SESSION_COOKIE_NAME = 'PEREGRINE_session'
 # verify project existence in dbgap or not
 VERIFY_PROJECT = False
