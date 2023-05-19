2f751a8d9b5131f63947f8b6f28c6821f1426ac6
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2023-05-19 16:06:59 -0500

    Log more errors in core metadata endpoint (#200)

--- peregrine/api.py
+++ peregrine/api.py
@@ -226,6 +226,6 @@ def run_for_development(**kwargs):
 
     try:
         app_init(app)
-    except:
+    except Exception:
         app.logger.exception("Couldn't initialize application, continuing anyway")
     app.run(**kwargs)
